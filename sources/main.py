99d0c42f28fe1efb136dc408363688ec7d10c69b
Mikhail Zotochev <mikhail@digitalcube.rs>
2022-06-26 17:18:22 +0200

    switched to webhook

--- sources/main.py
+++ sources/main.py
@@ -1,12 +1,24 @@
+import os
 from aiogram import executor
 
 from telegram_bot.bot_init import dp
 from telegram_bot.handlers import commands
 
 
+# webhook settings
+WEBHOOK_HOST = os.getenv('WEBHOOK_HOST')
+WEBHOOK_PATH = os.getenv('WEBHOOK_PATH')
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+
 def main():
     commands.register_handlers_commands(dp)
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_webhook(
+                           dispatcher=dp,
+                           webhook_path=WEBHOOK_PATH,
+                           skip_updates=True,
+    )
+    # executor.start_polling(dp, skip_updates=True)
 
 
 if __name__ == "__main__":
