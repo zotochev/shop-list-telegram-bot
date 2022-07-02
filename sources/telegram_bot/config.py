a2ab317180bfd861ab7d006bfe2c2a62a49fc318
Mikhail Zotochev <mikhail@digitalcube.rs>
2022-07-02 15:05:32 +0200

    fix

--- sources/telegram_bot/config.py
+++ sources/telegram_bot/config.py
@@ -25,7 +25,8 @@ ADMINS = os.getenv('ADMINS', None)
 
 def prepare_for_md(text: str) -> str:
     for c in "_*[]()~`>#+-=|{}.!":
-        return text.replace(c, f'{chr(47)}{c}')
+        text = text.replace(c, f'{chr(47)}{c}')
+    return text
 
 
 REACTIONS = {
